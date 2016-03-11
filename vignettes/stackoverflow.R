2f64211b086f027609b89f8be18ac7367c393e20
Jim Hester <james.f.hester@gmail.com>
2016-03-11 00:40:20 +0000

    version 1.1.1

--- vignettes/stackoverflow.R
+++ vignettes/stackoverflow.R
@@ -1,11 +1,26 @@
-## ----cache=FALSE, include=FALSE------------------------------------------
+#' ---
+#' title: "Stackoverflow Usage Examples"
+#' author: "Jim Hester"
+#' date: "`r Sys.Date()`"
+#' output: rmarkdown::html_vignette
+#' vignette: >
+#'   %\VignetteIndexEntry{Stackoverflow Usage Examples}
+#'   %\VignetteEngine{knitr::rmarkdown}
+#'   \usepackage[utf8]{inputenc}
+#' ---
+
+#+ cache=FALSE, include=FALSE
 knitr::opts_chunk$set(collapse = TRUE, comment = "#>")
 knitr::render_markdown(strict = TRUE)
 
-## ------------------------------------------------------------------------
+#' ### [http://stackoverflow.com/questions/27106552][]
+
+#' Using [rex](http://cran.r-project.org/web/packages/rex/index.html) may make this type of task a little simpler.
+
 x <- c("a=1", "b=3", "a=9", "c=2", "b=4", "a=2")
 
-## ------------------------------------------------------------------------
+#' First extract the names and values from the strings.
+
 library(rex)
 matches <- re_matches(x,
   rex(
@@ -15,11 +30,15 @@ matches <- re_matches(x,
     ))
 matches
 
-## ------------------------------------------------------------------------
+#' Then tally the groups using `split()`.
+
 groups <- split(as.numeric(matches$value), matches$name)
 groups
 
-## ------------------------------------------------------------------------
+#' If we try to convert directly to a data.frame from `split()` the groups with
+#' fewer members will have their members recycled rather than `NA`, so instead
+#' explicitly fill with `NA`.
+
 largest_group <- max(sapply(groups, length))
 largest_group
 
@@ -31,10 +50,14 @@ groups <- lapply(groups, function(group) {
 })
 groups
 
-## ------------------------------------------------------------------------
+#' Finally we can create the data.frame
+
 do.call("data.frame", groups)
 
-## ------------------------------------------------------------------------
+#' ### [http://stackoverflow.com/questions/14146362/][] ###
+
+#' Using [rex](http://cran.r-project.org/web/packages/rex/index.html) may make this type of task a little simpler.
+
 mystrings <- c("X2/D2/F4",
                "X10/D9/F4",
                "X3/D22/F4",
@@ -50,7 +73,9 @@ matches <- re_matches(mystrings,
   )
 as.numeric(matches$numbers)
 
-## ------------------------------------------------------------------------
+#' ### [http://stackoverflow.com/questions/8613237/][] ##
+#' Using [rex](http://cran.r-project.org/web/packages/rex/index.html) may make this type of task a little simpler.
+
 j <- "What kind of cheese isn't your cheese? (wonder) Nacho cheese! (groan) (Laugh)"
 
 library(rex)
@@ -62,7 +87,9 @@ matches <- re_matches(j,
   global = TRUE)
 matches
 
-## ------------------------------------------------------------------------
+#' ### [http://stackoverflow.com/questions/22976472][]
+#' Using [rex](http://cran.r-project.org/web/packages/rex/index.html) may make this type of task a little simpler.
+
 txt <- as.character("this is just a test! i'm not sure if this is O.K. or if it will work? who knows. regex is sorta new to me..  There are certain cases that I may not figure out??  sad!  ^_^")
 
 re <- rex(
@@ -81,7 +108,9 @@ re <- rex(
 
 re_substitutes(txt, re, "\\U\\1\\E\\2", global = TRUE)
 
-## ------------------------------------------------------------------------
+#' ### [http://stackoverflow.com/questions/27172007][]
+#' Using [rex](http://cran.r-project.org/web/packages/rex/index.html) may make this type of task a little simpler.
+
 x <- data.frame(
   locationid = c(
     1073744023,
@@ -128,13 +157,15 @@ re <-
 
 re_matches(x$address, re)
 
-## ------------------------------------------------------------------------
+#' ### [http://stackoverflow.com/questions/27155297/][]
+#' Using [rex](http://cran.r-project.org/web/packages/rex/index.html) may make this type of task a little simpler.
+
 library(rex)
 x <- c(
 "https://support.google.com/blogger/topic/12457
 https://support.google.com/blogger/topic/12457.
 https://support.google.com/blogger/topic/12457] 
-
+<<https://support.google.com/blogger/topic/12457>>
 https://support.google.com/blogger/topic/12457,
 https://support.google.com/blogger/topic/12457),
 xxxxxxhttps://support.google.com/blogger/topic/12457),hhhththta")
@@ -147,7 +178,8 @@ re <- rex(
 
 re_matches(x, re, global = TRUE)[[1]]
 
-## ------------------------------------------------------------------------
+#' ### [http://stackoverflow.com/questions/27219421][]
+#' Using [rex](http://cran.r-project.org/web/packages/rex/index.html) may make this type of task a little simpler.
 tmp <- c("Little Street","A323", "Essex Road (A43)", "M43","Orange street","M4","B2045","New Street")
 
 library(rex)
@@ -167,7 +199,9 @@ classify_road <- function(x) {
 
 classify_road(tmp)
 
-## ------------------------------------------------------------------------
+#' ### [http://stackoverflow.com/questions/22247410][]
+#' Using [rex](http://cran.r-project.org/web/packages/rex/index.html) may make this type of task a little simpler.
+
 x <- "this is a multiline text 
           some more test here 
           before we get to the good stuff 
@@ -178,7 +212,9 @@ re_matches(x, re)
 
 regexpr(re, x, perl = TRUE)
 
-## ------------------------------------------------------------------------
+#' ### [http://stackoverflow.com/questions/23447261][]
+#' Using [rex](http://cran.r-project.org/web/packages/rex/index.html) may make this type of task a little simpler.
+
 x = structure(list(text = structure(c(4L, 6L, 1L, 2L, 5L, 3L), .Label =     c("ãããæããããéãããæãããInappropriate announce:-(", 
 "@AirAsia your direct debit (Maybank) payment gateways is not working. Is it something     you are working to fix?", 
 "@AirAsia Apart from the slight delay and shortage of food on our way back from Phuket, both flights were very smooth. Kudos :)", 
@@ -200,7 +236,9 @@ re_matches(x$text,
   ),
   global = T)
 
-## ------------------------------------------------------------------------
+#' ### [http://stackoverflow.com/questions/27234040][]
+#' Using [rex](http://cran.r-project.org/web/packages/rex/index.html) may make this type of task a little simpler.
+
 z <- "<TABLE ALIGN=\"RIGHT\" BORDER CELLSPACING=\"0\" CELLPADDING=\"0\">
    <CAPTION><B>MESA HIGH VICTORIES</B></CAPTION>
    <TH>Team</TH>
@@ -222,7 +260,9 @@ re_matches(z,
     )
   ), options="single-line")
 
-## ------------------------------------------------------------------------
+
+#' ### [http://stackoverflow.com/questions/27236435][]
+#' Using [rex](http://cran.r-project.org/web/packages/rex/index.html) may make this type of task a little simpler.
 x <- "John a11|a12|\n  Ana a21|a22|\n  Jake a31|a23|\n   "
 
 re_matches(x,
@@ -234,7 +274,8 @@ re_matches(x,
       any_spaces),
   global = TRUE)[[1]]
 
-## ------------------------------------------------------------------------
+#' ### [http://stackoverflow.com/questions/25764839][]
+#' Using [rex](http://cran.r-project.org/web/packages/rex/index.html) may make this type of task a little simpler.
 x <- "MSGSRRKATPASRTRVGNYEMGRTLGEGSFAKVKYAKNTVTGDQAAIKILDREKVFRHKMVEQLKREISTMKLIKHPNVVEIIEVMASKTKIYIVLELVNGGELFDKIAQQGRLKEDEARRYFQQLINAVDYCHSRGVYHRDLKPENLILDANGVLKVSDFGLSAFSRQVREDGLLHTACGTPNYVAPEVLSDKGYDGAAADVWSCGVILFVLMAGYLPFDEPNLMTLYKRICKAEFSCPPWFSQGAKRVIKRILEPNPITRISIAELLEDEWFKKGYKPPSFDQDDEDITIDDVDAAFSNSKECLVTEKKEKPVSMNAFELISSSSEFSLENLFEKQAQLVKKETRFTSQRSASEIMSKMEETAKPLGFNVRKDNYKIKMKGDKSGRKGQLSVATEVFEVAPSLHVVELRKTGGDTLEFHKVCDSFYKNFSSGLKDVVWNTDAAAEEQKQ"
 re_matches(x,
   rex(
@@ -254,7 +295,8 @@ locs <- re_matches(x,
 
 substring(x, locs$start - 6, locs$end + 6)
 
-## ------------------------------------------------------------------------
+#' ### [http://stackoverflow.com/questions/15954171][]
+#' Using [rex](http://cran.r-project.org/web/packages/rex/index.html) may make this type of task a little simpler.
 x <- c("System configuration: lcpu=96 mem=196608MB ent=16.00")
 
 library(rex)
@@ -266,10 +308,11 @@ val <- as.numeric(
     )$ent
   )
 
-## ------------------------------------------------------------------------
+#' ### [http://stackoverflow.com/questions/27273996][]
+#' Using [rex](http://cran.r-project.org/web/packages/rex/index.html) to construct the regular expression may make it more understandable.
 x <- c("_A00_A1234B_", "_A00_A12345B_", "_A1_A12345_")
 
-## ------------------------------------------------------------------------
+#' approach #1, assumes always is between the second underscores.
 re_matches(x,
   rex(
     "_",
@@ -280,7 +323,7 @@ re_matches(x,
   )
 )
 
-## ------------------------------------------------------------------------
+#' approach #2, assumes an alpha, followed by 4 or 5 digits with a possible trailing alpha.
 re_matches(x,
   rex(
     capture(
@@ -291,7 +334,8 @@ re_matches(x,
   )
 )
 
-## ------------------------------------------------------------------------
+#' ### [http://stackoverflow.com/questions/27238323][]
+#' Using [rex](http://cran.r-project.org/web/packages/rex/index.html) may make this type of task a little simpler.
 string = c("ABC3JFD456", "ARST4DS324")
 
 re_matches(string,
@@ -300,7 +344,8 @@ re_matches(string,
     )
   )
 
-## ------------------------------------------------------------------------
+#' ### <http://stackoverflow.com/questions/27252250> ###
+#' Using [rex](http://cran.r-project.org/web/packages/rex/index.html) may make this type of task a little simpler.
 df <- structure(list(Object = c("T00055", "T00055", "E00336", "E00336",
 "E00336", "E00336", "T 00054"), Coding = c("T 00055_005_<002_+",
 "T 00055_008_<002_+", "E 00336_041_<001_+001_+", "E 00336_041_<001_+001_+001_+",
@@ -311,7 +356,9 @@ df <- structure(list(Object = c("T00055", "T00055", "E00336", "E00336",
 
 subset(df, grepl(rex(at_least(group("_+", anything), 2)), Coding))
 
-## ------------------------------------------------------------------------
+#' ### <http://stackoverflow.com/questions/27195734>
+#' Using [rex](http://cran.r-project.org/web/packages/rex/index.html) may make this type of task a little simpler.
+
 ids <- c("367025001", "CT_341796001", "M13X01692-01", "13C025050901", "13C00699551")
 
 re_substitutes(ids,
@@ -319,7 +366,8 @@ re_substitutes(ids,
   "",
   global = TRUE)
 
-## ------------------------------------------------------------------------
+#' ### <http://stackoverflow.com/questions/27237233>
+#' Using [rex](http://cran.r-project.org/web/packages/rex/index.html) may make this type of task a little simpler.
 library("rvest")
 library("stringr")
 
@@ -331,16 +379,19 @@ bodytext <- minimal %>%
 
 re_substitutes(bodytext, rex(spaces), "", global = TRUE)
 
-## ----message=FALSE-------------------------------------------------------
+#' ### <http://stackoverflow.com/questions/27227229>
+#+ message=FALSE
 string <- "this\\(system) {is} [full]."
 library(Hmisc)
 gsub("\\\\(.)", "\\1", escapeRegex(string))
 
-## ------------------------------------------------------------------------
+#' Alternatively [rex](http://cran.r-project.org/web/packages/rex/index.html) may make this type of task a little simpler.
 library(rex)
 re_substitutes(escape(string), rex("\\", capture(any)), "\\1", global = TRUE)
 
-## ------------------------------------------------------------------------
+#' ### <http://stackoverflow.com/questions/27317497>
+#' [rex](http://cran.r-project.org/web/packages/rex/) has a [vignette for parsing server logs](http://cran.r-project.org/web/packages/rex/vignettes/log_parsing.html). While the format is not exactly the same as your log you should be able to adapt it to your case fairly easily.
+#' As far as reading the log in assuming the file fits in memory your best bet is to read the whole file first with `readLines()`, then the following will put each field into a `data.frame` column.
 x <- "Feb  6 12:14:14 localhost haproxy[14389]: 10.0.1.2:33317 [06/Feb/2009:12:14:14.655] http-in static/srv1 10/0/30/69/109 200 2750 - - ---- 1/1/1/1/0 0/0 {1wt.eu} {} \"GET /index.html HTTP/1.1\""
 library(rex)
 re <- rex(
@@ -412,7 +463,9 @@ re <- rex(
 
 re_matches(x, re)
 
-## ------------------------------------------------------------------------
+#' ### <http://stackoverflow.com/questions/27422350/extract-character-preceding-first-dot-in-a-string>
+#' Using [rex](http://cran.r-project.org/web/packages/rex/index.html) may make this type of task a little simpler.
+
 my.data <- read.table(text = '
      my.string  state
      .........    A
@@ -429,14 +482,17 @@ library(rex)
 re_matches(my.data$my.string,
   rex(capture(except(".")), "."))$'1'
 
-## ------------------------------------------------------------------------
+#' ### <http://stackoverflow.com/questions/27410736>
+#' Using [rex](http://cran.r-project.org/web/packages/rex/index.html) may make this type of task a little simpler.
 string <- "Shakira - Wolf - 02.Hips don't lie.mp3"
 
 library(rex)
 re_matches(string,
   rex(capture(zero_or_more(any, type='lazy')), spaces, "-"))$'1'
 
-## ------------------------------------------------------------------------
+#' ### <http://stackoverflow.com/questions/27400286/>
+#' Using [rex](http://cran.r-project.org/web/packages/rex/index.html) may make this type of task a little simpler.
+
 string <- "I t is tim e to g o" 
 library(rex)
 re_substitutes(string, rex(
@@ -447,7 +503,9 @@ re_substitutes(string, rex(
       )
     ), "", global = TRUE)
 
-## ------------------------------------------------------------------------
+#' ### <http://stackoverflow.com/questions/27553126>
+#' Using [rex](http://cran.r-project.org/web/packages/rex/index.html) may make this type of task a little simpler.
+
 string <- "01:04:43.064 [12439] <2> xyz
 01:04:43.067 [12439] <2> a lmn
 01:04:43.068 [12439] <4> j klm
@@ -465,4 +523,3 @@ re <- rex(timestamp, space,
           capture(anything))
 
 re_matches(string, re, global = TRUE)
-
